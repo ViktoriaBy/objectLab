8ab1731b54a30d322b92847607072193da6ea8cd
ViktoriaB <v.chamborant@hotmail.com>
2022-01-11 13:44:56 -0600

    Solved Problem 3 & 4

--- lab.js
+++ lab.js
@@ -32,14 +32,14 @@ const dog = {
 //////////////////////////// PROBLEM 3 ////////////////////////////
 /* Print out the name of the dog you created in problem 2 using dot-notation. */
 
-//Code here
+console.log(dog.name)
 
 
 
 //////////////////////////// PROBLEM 4 ////////////////////////////
 /* Print out the color of the dog you created in problem 2 using bracket-notation. */
 
-//Code here
+console.log(dog['color'])
 
 
 
