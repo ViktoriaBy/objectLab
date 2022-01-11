24115b3f9ce46fe52acbe94a894ec1177d9af184
ViktoriaB <v.chamborant@hotmail.com>
2022-01-11 13:51:37 -0600

    Solved Problem 5

--- lab.js
+++ lab.js
@@ -60,18 +60,29 @@ const favoriteThings = {
 }
 
 /*
-  After you've made your object, use bracket or dot notation to add another key named 'car' with the value being your favorite car and then another key named 'show' with the value being your favorite show.
+  After you've made your object, use bracket or dot notation to add another key named 
+  'car' with the value being your favorite car and then another key named 
+  'show' with the value being your favorite show.
 */
 
-//Code here
+favoriteThings.car = "Ram"
+favoriteThings.show = "How I met youre mother"
+
+//console.log(favoriteThings.show)
 
 
 /*
-  Now use bracket or dot notation to change the value of the food key in your favoriteThings object to be 'Chicken Nuggets'
-  and change the value of the book key in your favoriteThings object to be 'Harry Potter'. (If they were either of those things already, change them to something else.)
+  Now use bracket or dot notation to change the value of the food key 
+  in your favoriteThings object to be 'Chicken Nuggets'
+  and change the value of the book key in your favoriteThings object 
+  to be 'Harry Potter'. (If they were either of those things already, 
+    change them to something else.)
 */
 
-//Code here
+favoriteThings.food = "Chicken Nuggets"
+favoriteThings.book = "Harry Potter"
+
+//console.log(favoriteThings.food)
 
 
 //////////////////////////// PROBLEM 6 ////////////////////////////
